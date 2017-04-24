c8cbf514204c8c1d35a52c85344bac727dddd4ba
ravijavi <jain.ravi654@gmail.com>
2017-04-23 20:41:33 -0400

    Update cookies.js
    cuz charlotte lel

--- js/cookies.js
+++ js/cookies.js
@@ -16,15 +16,18 @@ function WriteCookie() {
 	nameC = escape(document.getElementById("fullname").value) + ";";
 	emailC = escape(document.getElementById("email").value) + ";";
 	usernameC = escape(document.getElementById("username").value) + ";";
-
 	document.cookie = "username=" + usernameC;
 	document.cookie = "name=" + nameC;
 	document.cookie = "email=" + emailC;
-
 	console.log("Made Cookie");
+		if (document.getElementById("fullname").value = "") {
+		alert("Enter your name to continue.");
+		return;
+	}
+	else {
 	ShowCookie();
+	}
 }
-
 
 function ShowCookie() {
 	var allcookies = document.cookie = document.cookie;
