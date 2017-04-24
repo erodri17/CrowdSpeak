7cbf0f28a438619fe999ed08c67ac7b285cd20ad
Charlotte Wright <junikp@yahoo.com>
2017-04-23 22:10:52 -0400

    read file

--- js/main.js
+++ js/main.js
@@ -227,6 +227,7 @@ function download() {
   var a = document.createElement('a');
   a.style.display = 'none';
   a.href = url;
+  document.cookie = "video=" + username + ".webm";
   a.download = username + '.webm';
   document.body.appendChild(a);
   a.click();
