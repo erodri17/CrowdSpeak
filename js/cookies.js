a34d6d45250c7fc33309bb166b98f52e6ae78d61
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2017-04-27 13:02:20 -0400

    Some changes have been made
    Some minor changes (mostly typos) have been fixed

--- js/cookies.js
+++ js/cookies.js
@@ -4,14 +4,50 @@
  * @Email:  elmer.rodriguez6@gmail.com
  * @Filename: cookies.js
  * @Last modified by:   elmerxavierrodriguez
- * @Last modified time: 04-23-2017 <05:40:13 PM>
+ * @Last modified time: 04-23-2017 <08:17:07 PM>
  * @Copyright: © 2017 ELMER RODRIGUEZ ALL RIGHTS RESERVED
  */
 
+//
+// var cookiearray;
+//
+// function setCookie(cname, cvalue, exdays) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//     var expires = "expires="+ d.toUTCString();
+//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+//
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+// 						console.log("getCookie first character is empty");
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+// 						console.log("getCookie index equals zero");
+//         }
+//     }
+//     return "";
+// }
+//
+//
+// function checkCookie() {
+//     var usernameC = getCookie("username");
+//     if (usernameC != "") {
+// 				console.log("checkCookie");
+//         alert("Welcome again " + usernameC);
+//     }
+// }``
 
-var cookiearray;
 
 function WriteCookie() {
+<<<<<<< Updated upstream
 	console.log("Start Cookies");
 	nameC = escape(document.getElementById("fullname").value) + ";";
 	emailC = escape(document.getElementById("email").value) + ";";
@@ -29,13 +65,37 @@ function WriteCookie() {
 		console.log("Made Cookie");
 		ShowCookie();
 	}
+=======
+  console.log("Start Cookies");
+  if (document.myform.customer.value == "") {
+    alert("Enter some value!");
+    return;
+  }
+  nameC = escape(document.getElementById("fullname").value) + ";"
+	document.cookie="fullname=" + nameC;
+	document.write ("fullname=" + fullname );
+  emailC = escape(document.getElementById("email").value) + ";";
+	document.cookie="emailC=" + emailC;
+	document.write ("emailC=" + emailC );
+  usernameC = escape(document.getElementById("username").value) + ";";
+	document.cookie="usernameC=" + usernameC;
+	document.write ("usernameC=" + usernameC );
+
+  document.cookie = "username=" + usernameC;
+  document.cookie = "name=" + nameC;
+  document.cookie = "email=" + emailC;
+  document.write = ("Everything is alright")
+
+  console.log("Made Cookie");
+  ShowCookie();
+>>>>>>> Stashed changes
 }
 
 function ShowCookie() {
 	var allcookies = document.cookie = document.cookie;
-	
+
 	cookiearray = allcookies.split(';');
-	
+
 	for(var i = 0; i < cookiearray.length; i++){
 		name = cookiearray[i].split('=')[0];
 		value = cookiearray[i].split('=')[1];
@@ -46,28 +106,5 @@ function ShowCookie() {
 
 function ReadCookie(){
 	var x = document.cookie;
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-
-function checkCookie() {
-    var usernameC = getCookie("username");
-    if (usernameC != "") {
-        alert("Welcome again " + usernameC);
-    }
+	console.log("Read Cookie");
 }
